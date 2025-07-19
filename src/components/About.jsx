462d51e9af5c1677e560f007e0a99392a1c6a271
PET_TRACKER <indraxipixel123@gmail.com>
2025-07-19 13:55:05 +0530

    doneee

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Tilt } from "react-tilt";
+import  Tilt  from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -33,6 +33,7 @@ const ServiceCard = ({ index, title, icon }) => (
       </div>
     </motion.div>
   </Tilt>
+  
 );
 
 const About = () => {
